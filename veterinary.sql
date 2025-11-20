d2a8bec28958fdde0df8059e8e9d635726e0eb1e
maicaangela884 <maicaangela884@gmail.com>
2025-11-20 18:46:55 +0800

    feat/add-column-registereddate

--- veterinary.sql
+++ veterinary.sql
@@ -128,4 +128,5 @@ VALUES  (1, 1, '2023-01-05 00:00:00', 1,'Health check', 'N/A', 'Regular checkup,
         (9, 9, '2023-05-02 00:00:00', 4,'Allergic reaction', 'Antihistamines','Allergic reaction due to food prescribed antihistamine'),
         (10, 10,'2023-05-20 00:00:00', 6,'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
 
-        
+ALTER TABLE owners
+ADD COLUMN registereddate DATE;
