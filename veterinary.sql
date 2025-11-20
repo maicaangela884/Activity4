cc47760e97a0f0086f2a2b27b7b0ea9c765d171d
angeldinglasan <angelcamatadinglasan@gmail.com>
2025-11-20 18:32:49 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -30,3 +30,11 @@ CREATE TABLE appointments (
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
 
+CREATE TABLE doctors(
+    doctorid INT PRIMARY KEY,
+    dfirstname VARCHAR(50),
+    dlastname VARCHAR(50),
+    specialty VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
